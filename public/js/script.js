d838efe285600c9682440089ff6057b10fd99314
unknown <wildai@CAGTAW202285.ca.deloitte.com>
2015-06-10 17:16:27 -0400

    front end fix on button disable

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,35 @@ $(document).ready(function () {
     $("#quantity").val(parseNumber($(this).text()));
   });
 });
+/*
+function checkEmail() {
+    var email = document.getElementById("email").value;
+    var confemail = document.getElementById("confemail").value;
+    if(email != confemail) {
+      document.getElementById("confemail-parent").className = document.getElementById('confemail-parent').className.replace(/\bno-error\b/, 'has-error');
+      document.getElementById('buy-btn').disabled = true;
+    } else {
+      document.getElementById("confemail-parent").className = document.getElementById('confemail-parent').className.replace(/\bhas-error\b/,'no-error');
+      document.getElementById('buy-btn').disabled = false;
+    }
+}*/
+
+//  Bind the event handler to the "submit" JavaScript event
+$('form').submit(function () {
+
+    // Get the Login Name value and trim it
+    var email = $.trim($('#email').val());
+    var confemail = $.trim($('#confemail').val());
+
+    // Check if empty of not
+    if (email  === '') {
+        alert('Please enter email');
+        return false;
+    }
+    //Check if email address matches
+    if (email  != confemail) {
+        alert('Please enter the email address correctly');
+        return false;
+    }
+
+});
